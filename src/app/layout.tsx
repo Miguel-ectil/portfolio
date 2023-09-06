6d7116889420fbdd6c0bd1818c9ce96ab61300cf
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-09-05 23:52:23 -0300

    [ADD and UPD] Criando o meu portfólio pouco a pouco

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import LocalFont from "next/font/local";
+import { Analytics } from "../components/analytics";
+
+export const metadata: Metadata = {
+	title: {
+		default: "chronark.com",
+		template: "%s | chronark.com",
+	}
+};
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+	src: "../../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
+});
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+			<head>
+				<Analytics />
+			</head>
+			<body
+				className={`bg-black ${
+					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+				}`}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
+
