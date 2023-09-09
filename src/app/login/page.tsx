adf5ab3c7546adf9086501eb0f117b8d8f8a9bba
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-09-09 17:34:30 -0300

    [ADD UPD] Adicionando e estilizando botão na tela de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -60,6 +60,9 @@ export default function Login() {
             <input className="shadow appearance-none border bg-gray-700  border-red-400 rounded lg:w-80 xl:w-96 py-2 px-3 text-gray-200 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
             <p className="text-red-500 text-xs italic">Please enter password.</p>
           </div>
+          <button className="bg-blue-500 hover:bg-blue-700 w-52 lg:w-80 xl:w-96 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Sign In
+          </button>
 
         </motion.div>
       </div>
