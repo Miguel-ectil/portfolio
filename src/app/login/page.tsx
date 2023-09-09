7f0b0c481143514a5029840493e5812754602df8
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-09-09 17:33:47 -0300

    [ADD UPD] Adicionando as labels dosinputs e estilizando elas

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -48,11 +48,15 @@ export default function Login() {
         >
           
           <div className="mt-6 mb-4">
-
+            <label className="block text-gray-200 text-sm font-bold mb-2">
+                Username
+            </label>
             <input className="shadow bg-gray-700 leading-tight appearance-none border rounded lg:w-80 xl:w-96 py-2 px-3 text-white focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
           </div>
           <div className="mb-6">
-   
+            <label className="block text-gray-200 text-sm font-bold mb-2" >
+                Password
+            </label>
             <input className="shadow appearance-none border bg-gray-700  border-red-400 rounded lg:w-80 xl:w-96 py-2 px-3 text-gray-200 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
             <p className="text-red-500 text-xs italic">Please enter password.</p>
           </div>
