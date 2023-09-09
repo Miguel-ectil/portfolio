b776b695ff435424669542f8d88828491a711463
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-09-09 17:31:19 -0300

    [ADD and UPD]  Adicionando card da tela de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,28 +10,28 @@ export default function Login() {
   const ref = useRef<any>(null);
   const [isInView, setIsInView] = useState(false);
   const currentDate = new Date()
-//   useEffect(() => {
-//     const observer = new IntersectionObserver(
-//       (entries) => {
-//         entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//           setIsInView(true);
-//         }
-//         });
-//       },
-//       { threshold: 0 }
-//     );
-//     observer.observe(ref.current);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsInView(true);
+        }
+        });
+      },
+      { threshold: 0 }
+    );
+    observer.observe(ref.current);
 
-//     return () => observer.disconnect();
-//   }, [setIsInView]);
+    return () => observer.disconnect();
+  }, [setIsInView]);
   return (
     <div className="container grid items-center justify-center min-h-screen px-3 mx-auto">
       {/* <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       /> */}
-      {/* <div
+      <div
         ref={ref}
         className="grid mt-28 md:mt-20 lg-mt-16 sm:mt-16"
         style={{
@@ -46,30 +46,9 @@ export default function Login() {
           whileHover={{ scale: 1.1 }}
           transition={{ type: "spring", stiffness: 400, damping: 10 }}
         >
-          <div className="text-4xl items-center relative flex flex-col duration-700">
-            <strong>Log in</strong>
-          </div>
-          <div className="mt-6 mb-4">
-            <label className="block text-gray-200 text-sm font-bold mb-2">
-                Username
-            </label>
-            <input className="shadow bg-gray-700 leading-tight appearance-none border rounded lg:w-80 xl:w-96 py-2 px-3 text-white focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-200 text-sm font-bold mb-2" >
-                Password
-            </label>
-            <input className="shadow appearance-none border bg-gray-700  border-red-400 rounded lg:w-80 xl:w-96 py-2 px-3 text-gray-200 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-            <p className="text-red-500 text-xs italic">Please enter password.</p>
-          </div>
-          <button className="bg-blue-500 hover:bg-blue-700 w-52 lg:w-80 xl:w-96 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-            Sign In
-          </button>
-          <p className="text-center text-gray-500 text-xs mt-8">
-            <p>&copy;Current Date: {currentDate.toDateString()}</p>
-          </p>
+          
         </motion.div>
-      </div> */}
+      </div>
     </div>
   )
 }
