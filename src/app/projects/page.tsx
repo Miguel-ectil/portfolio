62e37bdc6c14699e980aabfdb1e26d9847725ef5
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-09-24 00:32:15 -0300

    [ADD and UPD] Melhorando o meu portfólio

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,8 +4,7 @@ import { motion } from "framer-motion";
 import { Navigation } from "@/components/navbar";
 import { Card } from "@/components/cards";
 import Link from "next/link";
-import { Github, Mail, Linkedin } from "lucide-react"; // Importe os ícones relevantes da Lucide-React
-
+import { Github, Mail, Linkedin } from "lucide-react"; 
 
 const socials = [
   {
@@ -95,64 +94,51 @@ export default function Example() {
               "transform 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s, opacity 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
           }}
         >
-          <motion.div
-            className="overflow-hidden relative border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/400 border-zinc-600 p-4 mb-20 mt-4"
-            whileHover={{ scale: 1.1 }}
-            transition={{ type: "spring", stiffness: 400, damping: 10 }}
-          >
-            <h1 className="text-4xl mb-2">Miguel Ectil</h1>
-            <p>
-              Olá! Sou um desenvolvedor front-end júnior apaixonado pela criação de experiências digitais incríveis. Meu nome é [Seu Nome], e estou animado em compartilhar minha paixão pela programação web e minha experiência em diversas tecnologias modernas. Com um foco especial em React, Next.js, TypeScript, JavaScript e muito mais, estou comprometido em contribuir para projetos de desenvolvimento web de alto nível.
-            </p>
-            <h2 className="text-3xl mb-1 mt-1">Experiência Profissional</h2>
-            <ul>
-              <li>
-                <span className="icon">
-                  {/* Substitua pelo ícone React */}
-                  {/* <ReactIcon size={20} /> */}
-                </span>
-                <span>React e Next.js: Tenho proficiência em desenvolver aplicativos web escaláveis e de alto desempenho utilizando o React.js e o Next.js. Isso inclui a criação de componentes reutilizáveis, gerenciamento de estado eficiente e otimização de desempenho.</span>
-              </li>
-              <li>
-                <span className="icon">
-                  {/* Substitua pelo ícone TypeScript */}
-                  {/* <TypeScriptIcon size={20} /> */}
-                </span>
-                <span>TypeScript e JavaScript: Estou confortável tanto com JavaScript quanto com TypeScript, o que me permite escrever código mais seguro, legível e robusto.</span>
-              </li>
-              <li>
-                <span className="icon">
-                  {/* Substitua pelo ícone UI/UX */}
-                  {/* <UIUXIcon size={20} /> */}
-                </span>
-                <span>UI/UX: Sou apaixonado por criar interfaces de usuário elegantes e funcionais. Minha experiência com Material UI, Tailwind CSS e Bootstrap me permite projetar e implementar designs atrativos.</span>
-              </li>
-              <li>
-                <span className="icon">
-                  {/* Substitua pelo ícone Vue.js */}
-                  {/* <VueIcon size={20} /> */}
-                </span>
-                <span>Vue.js: Embora minha principal expertise seja em React, também tenho familiaridade com o Vue.js e estou disposto a aprender novas tecnologias rapidamente.</span>
-              </li>
-              <li>
-                <span className="icon">
-                  {/* Substitua pelo ícone Git */}
-                  <Github  size={20} />
-                </span>
-                <span>Git e GitHub: Tenho experiência sólida em controle de versão com Git e uso o GitHub para colaborar em projetos de código aberto e em equipes de desenvolvimento.</span>
-              </li>
-              <li>
-                <span className="icon">
-                  {/* Substitua pelo ícone Linux */}
-                  {/* <LinuxIcon size={20} /> */}
-                </span>
-                <span>Ambiente Linux: Sou um entusiasta do Linux e estou familiarizado com diversos sistemas operacionais baseados em Linux, o que me ajuda a criar ambientes de desenvolvimento eficientes.</span>
-              </li>
-            </ul>
-            <address>
-              <p>Miguel Ectil <strong>São Paulo SP</strong></p>
-            </address>
-          </motion.div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-x-4"> {/* grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 */}
+            <motion.div
+              className="overflow-hidden relative border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/400 border-zinc-600 p-4 mb-4 mt-4"
+              // whileHover={{ scale: 1.1 }}
+              // transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
+              <h1 className="text-4xl mb-2">Miguel Ectil</h1>
+              <p>
+                Olá! Sou um desenvolvedor front-end júnior apaixonado pela criação de experiências digitais incríveis. Meu nome é [Seu Nome], e estou animado em compartilhar minha paixão pela programação web e minha experiência em diversas tecnologias modernas. Com um foco especial em React, Next.js, TypeScript, JavaScript e muito mais, estou comprometido em contribuir para projetos de desenvolvimento web de alto nível.
+              </p>
+
+            </motion.div>
+            <div className="grid grid-cols-1"> {/* grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 */}
+              <motion.div
+                className="overflow-hidden relative border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/400 border-zinc-600 p-4  mt-4"
+                // whileHover={{ scale: 1.1 }}
+                // transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              >
+                <h1 className="text-4xl mb-2">Miguel Ectil</h1>
+                <p>
+                  Olá! Sou um desenvolvedor front-end júnior apaixonado pela criação de experiências digitais incríveis. Meu nome é [Seu Nome], e estou animado em compartilhar minha paixão pela programação web e minha experiência em diversas tecnologias modernas. 
+                </p>
+              </motion.div>
+              <motion.div
+                className="overflow-hidden relative border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/400 border-zinc-600 p-4 mb-4 mt-4"
+                // whileHover={{ scale: 1.1 }}
+                // transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              >
+                <h1 className="text-4xl mb-2">Miguel Ectil</h1>
+                <p>
+                  Olá! Sou um desenvolvedor front-end júnior apaixonado pela criação de experiências digitais incríveis.
+                </p>
+              </motion.div>
+            </div>
+            {/*<motion.div
+              className="overflow-hidden relative border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/400 border-zinc-600 p-4 mb-20 "
+              // whileHover={{ scale: 1.1 }}
+              // transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
+              <h1 className="text-4xl mb-2">Miguel Ectil</h1>
+              <p>
+                Olá! Sou um desenvolvedor front-end júnior apaixonado pela criação de experiências digitais incríveis. Meu nome é [Seu Nome], e estou animado em compartilhar minha paixão pela programação web e minha experiência em diversas tecnologias modernas. Com um foco especial em React, Next.js, TypeScript, JavaScript e muito mais, estou comprometido em contribuir para projetos de desenvolvimento web de alto nível.
+              </p>
+            </motion.div> */}
+          </div>
           <SocialIcons />
         </div>
       </div>
