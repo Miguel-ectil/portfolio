a937a243005fc5dc6e40d3affec3e3f4a6c1a659
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-10-09 17:04:40 -0300

    [ADD] TEstando código

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -84,6 +84,26 @@ export default function Example() {
     <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
       <div className="container grid items-center justify-center min-h-screen px-3 mx-auto">
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mt-28 md:mt-20">
+        <img className="w-56 h-24 md:w-56 md:h-auto md:rounded-none rounded-full mx-auto" src="my-foto.jpeg" alt="" width="384" height="512" />
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              Sarah Dayan
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure>
         <div
           ref={ref}
           className="grid mt-28 md:mt-20 lg-mt-16 sm:mt-16"
