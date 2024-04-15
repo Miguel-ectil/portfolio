d49623654bfc0b39b13e1556380588925180bcce
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-04-15 17:51:03 -0300

    [ADD and UPD] Componentizano projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,13 @@ import Particles from '@/hooks/particles'
 import Image from 'next/image'
 import AboutMe from '@/components/about_me/page'
 import Contact from '@/components/contact/page'
+import Projects from '@/components/projects/page'
 
 export default function Home() {
   return (
     <>
       <div className='relative overflow-hidden bg-gradient-to-b  bg-[#171023]'>
         <Header />
-        {/* <Particles
-          className="absolute inset-0 z-50 animate-fade-in"
-          quantity={200}
-        /> */}
         <main className='pb-10 pl-4 lg:pl-16 flex flex-col lg:flex-row lg:items-center pt-[70px] lg:pt-0 z-40 relative'>
           <div className='lg:w-1/2 lg:pr-10'>
             <div className='flex flex-col space-y-3 py-2 md:space-y-4 lg:h-[38vh] lg:justify-end'>
@@ -58,80 +55,19 @@ export default function Home() {
           <div className='lg:w-1/2 lg:p-10 flex justify-center items-center order-first lg:order-last'>
             <Image
               src={`/Linha.svg`} 
-              width={700} // Ajuste o tamanho conforme necessário
-              height={700} // Ajuste o tamanho conforme necessário
+              width={700} 
+              height={700}
               alt='MID'
-              className='rounded-lg' // Adicione uma classe para arredondar a imagem, se necessário
+              className='rounded-lg'
             />
           </div>
         </main>
         <AboutMe />
-        <div className='flex justify-center mt-10'>
-          <div className='flex-col px-4'>
-            <p className='flex justify-center font-bold text-[#7DFFAF]'>
-              Projects
-            </p>
-            <h1 className='xl:text-3xl lg:text-2xl font-bold'>
-            Take a look at my highlighted projects            
-            </h1>
-          </div>
-        </div>
-        <div className='flex px-4 lg:px-14 xl:px-20 gap-6 lg:gap-6 xl:gap-6 mt-8 flex-col lg:flex-row'>
-          <div className='flex rounded-xl p-4 bg-[#2C243B] '>
-            <div className='lg:w-1/2 lg:p-10 flex '>
-              <Image
-                src={`/bro.svg`} 
-                width={700} 
-                height={700}
-                alt='MID'
-                className='rounded-lg'
-              />
-            </div>
-          </div>
-
-          <div className='flex rounded-xl p-4 bg-[#2C243B] '>
-            <div className='lg:w-1/2 lg:p-10 flex '>
-              <Image
-                src={`/bro.svg`} 
-                width={700} 
-                height={700}
-                alt='MID'
-                className='rounded-lg'
-              />
-            </div>
-          </div>
-
-          <div className='flex rounded-xl p-4 bg-[#2C243B]'>
-            <div className='lg:w-1/2 lg:p-10 flex '>
-              <Image
-                src={`/bro.svg`} 
-                width={700} 
-                height={700}
-                alt='MID'
-                className='rounded-lg'
-              />
-            </div>
-          </div>
-        </div>
-        <div className='flex justify-center mt-6'>
-          <button 
-            className='md:text-xl flex cursor-pointer items-center gap-x-2 rounded-lg bg-[#413A4F] px-5 py-1.5 text-sm font-semibold text-white transition hover:opacity-75 md:px-4 md:py-2.5'
-          >          
-            See all
-            <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              fill="none" viewBox="0 0 24 24" 
-              strokeWidth={1.5} stroke="currentColor" 
-              className="w-6 h-6"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-            </svg>
-          </button>
-        </div>
+        <Projects />
         <Contact />
         <Footer />
         <Particles
-          className="absolute inset-0 z-0 animate-fade-in" // Altere a classe z-50 para z-0
+          className="absolute inset-0 z-0 animate-fade-in" 
           quantity={200}
         />
       </div>
